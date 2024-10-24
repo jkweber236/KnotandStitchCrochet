787005c99549522f40b3ab3fca050f75ff799e9e
dalanweber9876 <dalanweber9876@gmail.com>
2024-10-24 14:09:58 -0600

    Updated scss files to css and added every page needed in the website

--- src/app/Homepage/homepage.component.ts
+++ src/app/Homepage/homepage.component.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.scss',
+  styleUrl: './homepage.component.css',
 })
 export class AppComponent {
   title = 'knotandstitchcrochet';
